d518eceedf6702712f82f9e29b407993e41357da
Robert <rsobieszek89@gmail.com>
2019-07-09 15:36:32 +0200

    Updates

---
+++ precache-manifest.cab465868973ce9f404f527b19341a04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec7015a04db1fc9768692db5d2fbcd5",
+    "url": "/Simple-ToDo-App/index.html"
+  },
+  {
+    "revision": "f47e784aaea2807ffb53",
+    "url": "/Simple-ToDo-App/static/css/main.3435c567.chunk.css"
+  },
+  {
+    "revision": "1af68d6023f25a8470a0",
+    "url": "/Simple-ToDo-App/static/js/2.21d3e144.chunk.js"
+  },
+  {
+    "revision": "f47e784aaea2807ffb53",
+    "url": "/Simple-ToDo-App/static/js/main.2d585924.chunk.js"
+  },
+  {
+    "revision": "57c3a39bd9d9abaa48eb",
+    "url": "/Simple-ToDo-App/static/js/runtime~main.37788316.js"
+  }
+]);
